bd70c2bcac263e181a4bf23e17ba79061971426f
Alan Alvarado <alvarado.aac@gmail.com>
2024-05-25 21:57:02 -0600

    Remplazo categorias para que queden como el backend

--- ecommerce-front/src/components/NavbarCategory.jsx
+++ ecommerce-front/src/components/NavbarCategory.jsx
@@ -4,25 +4,34 @@ function NavbarCategory () {
   return (
     <Nav fill variant='tabs' defaultActiveKey='/home'>
       <Nav.Item>
-        <Nav.Link eventKey='link-1'>Electrodomesticos</Nav.Link>
+        <Nav.Link eventKey='link-1'>Electrónica</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey='link-2'>Hogar</Nav.Link>
+        <Nav.Link eventKey='link-2'>Moda y Accesorios</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey='link-3'>Bebés</Nav.Link>
+        <Nav.Link eventKey='link-3'>Belleza y Cuidado Personal</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey='link-4'>Ropa</Nav.Link>
+        <Nav.Link eventKey='link-4'>Hogar y Cocina</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey='link-5'>Libros</Nav.Link>
+        <Nav.Link eventKey='link-5'>Deportes</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey='link-6'>Alimentos</Nav.Link>
+        <Nav.Link eventKey='link-6'>Salud y Bienestar</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey='link-7'>Bebidas</Nav.Link>
+        <Nav.Link eventKey='link-7'>Automotriz</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey='link-8'>Mascotas</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey='link-9'>Alimentos y Bebidas</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey='link-10'>Libros y Papelería</Nav.Link>
       </Nav.Item>
     </Nav>
   )
