1e146a08bad3fd6e6ffcc78aa3a8e710aef5d89a
Alan Alvarado <alvarado.aac@gmail.com>
2024-05-31 21:14:12 -0600

    Agrego carpeta al componente header, para poner estilos

--- ecommerce-front/src/main.jsx
+++ ecommerce-front/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from './components/Header.jsx'
+import Header from './components/Header/Header.jsx'
 import NavbarCategory from './components/NavbarCategory.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
