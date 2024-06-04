96972f7fe62915de65aa3db77ed23563c1fd81fe
katinavalle9 <katinavalle9@gmail.com>
2024-06-03 22:29:42 -0400

    pagina principal con filtros

--- ecommerce_FF/src/components/Navbar/NavbarCategory.jsx
+++ ecommerce_FF/src/components/Navbar/NavbarCategory.jsx
@@ -1,30 +1,43 @@
-import Nav from 'react-bootstrap/Nav'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import axios from "axios";
 
-function NavbarCategory () {
-  const [categories, setCategories] = useState([])
+function NavbarCategory({ onCategorySelect }) {
+  const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    axios.get('http://127.0.0.1:3000/api/v1/categories')
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/categories`)
       .then((response) => {
-        setCategories(response.data)
+        setCategories(response.data);
       })
-      .catch(console.error)
-  }, [])
-  return (
-    <Nav fill variant='tabs' defaultActiveKey='/home'>
-      {
-        categories.map((category) => {
-          return (
-            <Nav.Item key={category._id}>
-              <Nav.Link eventKey='link-1'>{category.name}</Nav.Link>
-            </Nav.Item>
-          )
-        })
-      }
+      .catch((error) => {
+        console.error('Error fetching categories:', error);
+      });
+  }, []);
 
+  const handleCategoryClick = (categoryId) => {
+    console.log('Category selected:', categoryId); // Verifica que se está seleccionando una categoría
+    onCategorySelect(categoryId);
+    navigate(`/items/${categoryId}`);
+  };
+
+  return (
+    <Nav fill variant='tabs' defaultActiveKey='/home' className="mt-5 flex-nowrap overflow-auto">
+      {categories.map((category) => (
+        <Nav.Item key={category._id}>
+          <Nav.Link 
+            eventKey={category._id} 
+            onClick={() => handleCategoryClick(category._id)}
+          >
+            {category.name}
+          </Nav.Link>
+        </Nav.Item>
+      ))}
     </Nav>
-  )
+  );
 }
 
-export default NavbarCategory
+export default NavbarCategory;
--- ecommerce_FF/src/main.jsx
+++ ecommerce_FF/src/main.jsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import Header from './components/Header/Header.jsx'
-import NavbarCategory from './components/Navbar/NavbarCategory.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <NavbarCategory />
     <App />
   </React.StrictMode>
-)
+);
