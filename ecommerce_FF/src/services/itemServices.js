97391d601f489aba22cbd78d5cd537ff1b496240
cris-zs <cristinaz0597@gmail.com>
2024-06-05 18:23:20 -0600

    change urls

--- ecommerce_FF/src/services/itemServices.js
+++ ecommerce_FF/src/services/itemServices.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = 'https://ecommerce-back-1ly7.onrender.com'
+const BASE_URL = 'https://ecommerce-back-1ly7.onrender.com/api/v1'
 
 const getOneItemService = (id) => axios.get(`${BASE_URL}/items/${id}`)
 
--- ecommerce_FF/src/services/userServices.js
+++ ecommerce_FF/src/services/userServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://ecommerce-back-1ly7.onrender.com'
+const BASE_URL = 'https://ecommerce-back-1ly7.onrender.com/api/v1'
 
 const registerUserServices = (data) => axios.post(`${BASE_URL}/user/register`, data)
 const logInUserServices = (data) => axios.post(`${BASE_URL}/user/login`, data)
