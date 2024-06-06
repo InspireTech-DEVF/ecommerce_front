389e0a316aab9f4adaf557c17a129ff2378f28c4
cris-zs <cristinaz0597@gmail.com>
2024-06-05 20:24:05 -0600

    Pagina secret

--- ecommerce_FF/src/routes/index.jsx
+++ ecommerce_FF/src/routes/index.jsx
@@ -3,14 +3,21 @@ import  Signup  from '../pages/Signup.jsx'
 import Login from '../pages/Login.jsx'
 import Profile from '../pages/Profile.jsx'
 import ItemDetail from '../pages/ItemDetail.jsx'
+import Secret from '../pages/Secret.jsx'
+import { useAuthContext } from '../hooks/useAuth.jsx'
 
 const RoutesIndex = () => {
+  const {isAuth} = useAuthContext()
   return (
     <Routes>
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/items/:id' element={<ItemDetail />} />
+         <Route
+        path='/secret'
+        element={isAuth ? <Secret /> : <Navigate to='/login' />}
+        /> 
     </Routes>
   )
 }
--- ecommerce_FF/src/services/itemServices.js
+++ ecommerce_FF/src/services/itemServices.js
@@ -2,8 +2,19 @@ import axios from "axios"
 
 const BASE_URL = 'https://ecommerce-back-1ly7.onrender.com/api/v1'
 
+const getAllItemsServices = () => axios.get(`${BASE_URL}/items`)
 const getOneItemService = (id) => axios.get(`${BASE_URL}/items/${id}`)
 
+
+const createItemServices = (data, jwtToken) => axios.post(`${BASE_URL}/items`,
+  data, {
+    headers: {
+      Authorization: `Bearer ${jwtToken}`
+    }
+  })
+
 export{
-    getOneItemService
+    getAllItemsServices,
+    getOneItemService,
+    createItemServices
 }
